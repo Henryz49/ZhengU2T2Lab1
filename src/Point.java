764ddcf39631f7e5f552cab2810c184d9bc56137
BT_3W2_19 <Henryz49@nycstudents.net>
2022-10-04 09:26:44 -0400

    a

--- src/Point.java
+++ src/Point.java
@@ -1,2 +1,57 @@
-package PACKAGE_NAME;public class Point {
+public class Point {
+
+    private int x;
+    private int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Point(int both) {
+        this.y = both;
+        this.x = both;
+    }
+
+    public Point() {
+        this.y = 0;
+        this.x = 0;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setX(int newX) {
+        this.x = newX;
+    }
+
+    public void setY(int newY) {
+        this.y = newY;
+    }
+
+    public String coordinate()
+    {
+        return "(" + x + "," + y + ")" ;
+    }
+
+    public String quadrant(){
+        String quadrant;
+
+    }
+
+
+
+
+
+
+
+
+
+
 }
+
